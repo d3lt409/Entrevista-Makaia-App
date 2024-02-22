b9c4f64f8907795243772d2c9e04c20b08208523
d3lt409 <manuelfer1996@gmail.com>
2024-02-22 18:05:48 -0500

    update service url

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURL = "https://entrevista-makaia-api.onrender.com/api/v1/";
 
-const apiUrl = "http://localhost:8080/api/v1/";
+// const apiUrl = "http://localhost:8080/api/v1/";
 
 export const axiosMain = axios.create({
   baseURL: apiURL,
