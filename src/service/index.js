67dfe3e344a2b57e20ddd91b97fd0b6e82af2361
d3lt409 <manuelfer1996@gmail.com>
2024-02-23 09:47:15 -0500

    Railwail deploy

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiURL = "https://entrevista-makaia-api.onrender.com/api/v1/";
+const apiURL = "https://equipo4-production-caa0.up.railway.app/api/v1/";
 
 // const apiUrl = "http://localhost:8080/api/v1/";
 
